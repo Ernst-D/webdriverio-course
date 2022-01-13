20d8fa06860ecc8a0cb27f9a60ef20b21668fc87
Ernst D <ernst.dzerevanko@gmail.com>
2022-01-13 16:47:16 +0300

    [step1] fixed login spec

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -4,10 +4,11 @@ describe("Test suite",()=>{
         let usernameInput = await $('#username');
         let passwordInput = await $("#password")
         await usernameInput.waitForDisplayed();
-        await usernameInput.setValue("Adolfo.Skiles80");
+        await usernameInput.setValue("Tavares_Barrows");
         await passwordInput.setValue("s3cret");
         await $('[data-test="signin-submit"]').waitForEnabled();
         await $('[data-test="signin-submit"]').click();
+        await $('[data-test="transaction-list"]').waitForDisplayed();
         await browser.saveScreenshot("./image.png");
     })
 })
